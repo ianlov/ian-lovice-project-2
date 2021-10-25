8cf12c51336989232a88d54ffb1b10aab80fefc6
Ian Lovice <ianlovice@Ians-MacBook-Air.local>
2021-10-25 17:06:10 -0400

    changed home page

--- sing-song-app/src/components/Home.jsx
+++ sing-song-app/src/components/Home.jsx
@@ -4,9 +4,11 @@ const Home = ({ gigs }) => {
 
   return (
     <div>
-      <h1>Introduction</h1>
-      <p>Vel praesentium similique qui ut labore laudantium. Ut aut similique quis in expedita iure sapiente eos. Fugiat rerum nam architecto perspiciatis quibusdam autem. Ea corporis laboriosam nam expedita atque beatae natus officiis. Suscipit vel modi porro non tempore corrupti ipsum nesciunt. Et laborum cumque numquam.</p>
-      <Link to="/gigs" >Search Gigs</Link>
+      <h1>Live Music Underground</h1>
+      <p>Welcome to Live Music Underground. We connect music lovers to live performers. Post or search.</p>
+      <Link to="/post" ><p>Post Gig</p></Link>
+      <Link to="/gigs" ><p>Search Gigs</p></Link>
+      <Link to="/about" ><p>About</p></Link>
     </div>
   )
 }
