dff8e716ea322c5bc76a079b54078d2dbfb41548
Ian Lovice <ianlovice@Ians-Air.lan>
2021-10-27 14:39:01 -0400

    got status to show on page

--- sing-song-app/src/components/Map.jsx
+++ sing-song-app/src/components/Map.jsx
@@ -1,20 +1,31 @@
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const render = (status: Status) => {
+  return <h1>{status}</h1>
+};
 
 
-const Map = () => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [map, setMap] = useState<google.maps.Map>();
+const Map = () => <Wrapper apiKey={"AIzaSyAZjycaiqkqixrYvRQhKXPJDi_4UhhmrhQ"} render={render} />;
 
-  useEffect(() => {
-    if (ref.current && !map) {
-      setMap (new window.google.maps.Map(ref.current))
-    }
-  }, [ref, map])
 
-  return (
-    <div className="map" ref={ref}></div>
-  )
-}
+{/* <Wrapper apiKey={"AIzaSyAZjycaiqkqixrYvRQhKXPJDi_4UhhmrhQ"} render={render} ></Wrapper>
+  const Map = (status: Status) => {
+    const ref = useRef<HTMLDivElement>(null)
+    const [map, setMap] = useState<google.maps.Map>();
+
+    useEffect(() => {
+      if (ref.current && !map) {
+        setMap (new window.google.maps.Map(ref.current))
+      }
+    }, [ref, map])
+
+    return (
+      <div className="map" ref={ref}></div>
+    )
+  }
+</Wrapper>
+
+export default Map; */}
 
 export default Map;
