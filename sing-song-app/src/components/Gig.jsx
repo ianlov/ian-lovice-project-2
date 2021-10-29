ad47b6c0c7e50b42c5673b374add314c7c98a73a
Ian Lovice <ianlovice@Ians-Air.lan>
2021-10-29 05:43:27 -0400

    Gig ready

--- sing-song-app/src/components/Gig.jsx
+++ sing-song-app/src/components/Gig.jsx
@@ -1,33 +1,5 @@
-import axios from "axios"
-import { useEffect, useState } from "react";
-
-
-const API_KEY = "keyLH1D8HDodG0Om3";
-const API_URL = `https://api.airtable.com/v0/app6U7NfwIM8GmQ47/Table%201?api_key=${API_KEY}`
 
 const Gig = ({ gig }) => {
-  const [likeCount, setLikeCount] = useState(0);
-  const [clicked, setClicked] = useState(false);
-  const [likeObj, setLikeObj] = useState({
-    records: [
-      {
-        id: gig.id,
-        fields: {
-          artist: gig.fields.artist,
-          address: gig.fields.address,
-          date: gig.fields.date,
-          link: gig.fields.link,
-          time: gig.fields.time,
-          city: gig.fields.city,
-          likes: gig.fields.likes,
-        }
-      }
-    ]
-  });
-
-
-  
-
   return (
     <div className="gig" >
       <h4>{gig.fields.artist} in {gig.fields.city}</h4>
@@ -35,7 +7,6 @@ const Gig = ({ gig }) => {
       <h5>{gig.fields.time}</h5>
       <h5>{gig.fields.address}</h5>
       <h5><a href={gig.fields.link} >Listen</a></h5>
-      <button onClick={addLike} >{clicked ? `${gig.fields.likes} will be there` : `${gig.fields.likes} will be there`}</button>
     </div>
   )
 }
