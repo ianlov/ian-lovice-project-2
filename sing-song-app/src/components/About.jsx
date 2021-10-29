574a8d30da2056390b596f5102ec9f6dddf1bdf2
Ian Lovice <ianlovice@Ians-Air.lan>
2021-10-29 05:59:57 -0400

    show gig and search ready:

--- sing-song-app/src/components/About.jsx
+++ sing-song-app/src/components/About.jsx
@@ -5,6 +5,12 @@ const About = () => {
     <div>
       <h1>About</h1>
       <p>This page was built as a General Assembly Software Engineering Immersive project. The goal was to create a space where live music performers could share their upcoming shows with people who love live music. Live Music Underground requires no login, so anyone can post or search.</p>
+      
+      <h3>See other projects</h3>
+      <ul>
+        <li><img src="./images/github.png" alt="github" /></li>
+        <li><img src="./images/linkedin.png" alt="linkedin" /></li>
+      </ul>
     </div>
   )
 }
--- sing-song-app/src/components/Search.jsx
+++ sing-song-app/src/components/Search.jsx
@@ -1,43 +1,38 @@
 import { useState } from "react";
 
-const Search = ({ gigs, setSearchGigs, setToggleFetch, toggleFetch, setToggleShowMap }) => {
+const Search = ({ gigs, setSearchGigs, setToggleFetch, toggleFetch }) => {
   const [search, setSearch] = useState("");
   const [toggleFocus, setToggleFocus] = useState(false)
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
-    
     setToggleFetch(!toggleFetch)
-
-    setToggleShowMap(true)
-
     const searchLowerCase = search.toLowerCase()
-
     const searchedGigs = gigs.filter(gig => {
       if (
-        gig.fields.artist.toLowerCase().includes(searchLowerCase) || 
-        gig.fields.city.toLowerCase().includes(searchLowerCase) || 
+        gig.fields.artist.toLowerCase().includes(searchLowerCase) ||
+        gig.fields.city.toLowerCase().includes(searchLowerCase) ||
         gig.fields.date.toLowerCase().includes(searchLowerCase)
-        ) { return gig } else { return null }
+      ) { return gig } else { return null }
     })
-  
+
     setSearchGigs(searchedGigs)
   }
 
   return (
     <div className="search-form" >
       <form onSubmit={handleSubmit} >
-        <input 
-          id="search" 
-          type="search" 
-          placeholder="artist, date, city..." 
-          onChange={(ev) => setSearch(ev.target.value)} 
+        <input
+          id="search"
+          type="search"
+          placeholder="artist, date, city..."
+          onChange={(ev) => setSearch(ev.target.value)}
           onFocus={() => setToggleFocus(true)}
           onBlur={() => setToggleFocus(false)}
         />
         <button><span class="material-icons">search</span></button>
       </form>
-      {toggleFocus ? 
+      {toggleFocus ?
         <p>Date "yyyy-mm-dd"</p> :
         <p></p>
       }
